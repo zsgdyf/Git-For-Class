e2f2752169ec37f5114e8ff071c0947ab63a4d85
一切随缘 <34258580+zsgdyf@users.noreply.github.com>
2018-10-16 20:10:21 +0800

    bfs function

--- ArtificialIntelligence/EightNumber.cpp
+++ ArtificialIntelligence/EightNumber.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 int getInversions(vector<int> num)
@@ -54,10 +56,117 @@ void printMatrix(vector<int> num)
     }
 }
 
+int zeroLocation(vector<int> num)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        if (num[i] == 0)
+        {
+            return i;
+        }
+    }
+}
+
+vector<int> moveUp(vector<int> num)
+{
+    int i = zeroLocation(num);
+    num[i] = num[i - 3];
+    num[i - 3] = 0;
+    return num;    
+}
+
+vector<int> moveDown(vector<int> num)
+{
+    int i = zeroLocation(num);
+    num[i] = num[i + 3];
+    num[i + 3] = 0;
+    return num;    
+}
+
+vector<int> moveLeft(vector<int> num)
+{
+    int i = zeroLocation(num);
+    num[i] = num[i - 1];
+    num[i - 1] = 0;
+    return num;    
+}
+
+vector<int> moveRight(vector<int> num)
+{
+    int i = zeroLocation(num);
+    num[i] = num[i + 1];
+    num[i + 1] = 0;
+    return num;    
+}
+
+void BFS(vector<int> start, vector<int> end)
+{
+    int steps = 0;
+    queue<vector<int>> open;
+    vector<vector<int>> close,state;
+    vector<int> now, nowMoveUp, nowMoveDown, nowMoveRight, nowMoveLeft;
+    open.push(start);
+    while (!open.empty())
+    { 
+        now = open.front();
+        open.pop();
+        state.push_back(now);
+        int zeroPosition = zeroLocation(now);
+        auto iter = find(close.begin(), close.end(), now);
+        if (!isEqual(now, end))// && iter == close.end())
+        {
+            if (iter == close.end())
+            {
+                if (zeroPosition % 3 != 0)
+            {
+                nowMoveLeft = moveLeft(now);
+                steps++;
+                open.push(nowMoveLeft);
+            }
+                if (zeroPosition > 3)
+            {
+                nowMoveUp = moveUp(now);
+                steps++;
+                open.push(nowMoveUp);
+            }
+            if (zeroPosition % 3 != 2)
+            {
+                nowMoveRight = moveRight(now);
+                steps++;
+                open.push(nowMoveRight);
+            }
+            if (zeroPosition < 5)
+            {
+                nowMoveDown = moveDown(now);
+                steps++;
+                open.push(nowMoveDown);
+            }
+            close.push_back(now);
+            }
+            else
+            {
+                continue;
+            }
+        }
+        else
+        {
+            cout << "steps:" << steps << endl;
+            for (vector<int> s:state)
+            {
+                cout << "---------" << endl;
+                printMatrix(s);
+                cout << "---------" << endl;
+            }
+            break;
+        }
+    }
+
+}
+
 int main()
 {
     vector<int> start, end;
-    start = {1, 3, 2, 4, 0, 5, 6, 7, 8};
+    start = {2, 8, 3, 1, 0, 4, 7, 6, 5};
     end = {1, 2, 3, 8, 0, 4, 7, 6, 5};
     cout << "Input start:" << endl;
     printMatrix(start);
@@ -81,6 +190,7 @@ int main()
     */
     if (hasSolution(start, end))
     {
+        BFS(start, end);
         cout << "Has solution";
     }
     else
